93dfc1de6133c81bade911822be09cdf80bf6f4e
Luigi213 <reyesgianluigi88@gmail.com>
2023-01-12 13:35:18 +0100

    setupstart vite-comics with App main

---
+++ src/components/CardaDc.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    props:{
+        dcImage: Object
+    } 
+}
+</script>
+<template lang="">
+    <div class="card">
+        {{ dcImage.series }}
+    </div>
+</template>
+<style lang="scss" scoped>
+    @use '../styles/partials/variables' as *;
+    @use '../styles/partials/mixins' as *;
+    .card{
+        color: $tertiary
+    }
+</style>
